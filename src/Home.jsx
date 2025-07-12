2d31794c7e6e7271828d6caf6e115d96ea16c184
David Y. Stephenson <david@davidystephenson.com>
2025-07-12 12:03:50 -0400

    Initialize counter at invite count

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import Counter from './Counter'
 
 export default function Home () {
+  const [count, setCount] = useState(0)
   const [list, setList] = useState([])
   useEffect(() => {
     async function download () {
       const response = await fetch('http://localhost:4000/invites')
       const data = await response.json()
       setList(data.invites)
+      setCount(data.invites.length)
     }
     download()
   }, [])
@@ -20,6 +21,9 @@ export default function Home () {
       </li>
     )
   })
+  function handleClick () {
+    setCount(count + 1)
+  }
   return (
     <>
       <h2>Home</h2>
@@ -27,7 +31,7 @@ export default function Home () {
         {items}
       </ul>
       <p>There are {list.length} invites</p>
-      <Counter />
+      <button onClick={handleClick}>Count: {count}</button>
     </>
   )
 }
